17441a39843e66969e034495668d8278c358fe9d
Pa_O_La <paola.mussida@gmail.com>
2020-04-15 20:40:46 +0200

    inizializzazione progetto

---
+++ 1_analisi.R
@@ -0,0 +1,24 @@
+#esplorazione dati
+
+mydir <- "C:/Users/10114518/Documents/dm/prj-dropout/mixedmodel/"
+mydirdi <- paste0(mydir,"datain/")
+mydirdo <- paste0(mydir,"dataout/")
+setwd(mydir)
+
+#carriere 1 livello
+carreers <- read.csv(paste0(mydirdi,'SPEET_POP_PLUS.csv'))
+#descrizione dei corsi di laurea
+degree_desc <- read.csv(paste0(mydirdi,'SPEET_DEGREE_INFORMATION.csv'))
+#esami
+exams <- read.csv(paste0(mydirdi,'SPEET_EXAMS.csv'))
+#statistiche degli esami
+exams_stats <- read.csv(paste0(mydirdi,'SPEET_EXAMS_STATS.csv'))
+#titolo medio
+prev_studies <- read.csv(paste0(mydirdi,'SPEET_PREV_STUDIES.csv'))
+#residenze e domicilio per capire se essere pensolari o fuori casa influisce
+home <- read.csv(paste0(mydirdi,'SPEET_RESIDENZE.csv'))
+#dati di mobilità
+mobility <- read.csv(paste0(mydirdi,'SPEET_MOBILITY.csv'))
+
+
+table(degree_desc)
