e509d14b3137171772f0835bdff5493082a995c6
Riccardo Bertoglio <ticio94@hotmail.it>
2020-04-18 17:33:19 +0200

    added new tables

--- 1_analisi.R
+++ 1_analisi.R
@@ -9,6 +9,12 @@ carreers <- read.csv(paste0(mydirdi,'SPEET_POP_PLUS.csv'))
 degree_desc <- read.csv(paste0(mydirdi,'SPEET_DEGREE_INFORMATION.csv'))
 #esami
 exams <- read.csv(paste0(mydirdi,'SPEET_EXAMS.csv'))
+#not passed exams
+exams_not_passed <- read.csv(paste0(mydirdi, 'SPEET_EXAMS_NOT_PASSED.csv'))
+# passed exams
+exams_passed <- read.csv(paste0(mydirdi, 'SPEET_EXAMS_PASSED.csv'))
+# passed exams aggregated table
+exams_passed_aggr <- read.csv(paste0(mydirdi, 'SPEET_EXAMS_PASSED_AGGR.csv'))
 #statistiche degli esami
 exams_stats <- read.csv(paste0(mydirdi,'SPEET_EXAMS_STATS.csv'))
 #titolo medio
