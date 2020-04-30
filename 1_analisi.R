cf2ef435abf06cd45da740f230fd627eea5e646c
Pa_O_La <paola.mussida@gmail.com>
2020-04-30 16:23:48 +0200

    data aggregation

--- 1_analisi.R
+++ 1_analisi.R
@@ -5,8 +5,7 @@ mydirdo <- paste0(getwd(),"/data/dataout/")
 
 #descrizione dei corsi di laurea
 degree_desc <- read.csv(paste0(mydirdi,'SPEET_DEGREE_INFORMATION.csv'))
-#esami
-exams <- read.csv(paste0(mydirdi,'SPEET_EXAMS.csv'))
+
 #not passed exams
 exams_not_passed <- read.csv(paste0(mydirdi, 'SPEET_EXAMS_NOT_PASSED.csv'))
 # passed exams
@@ -23,11 +22,24 @@ careers <- read.csv(paste0(mydirdi,'SPEET_POP_PLUS.csv'))
 prev_studies <- read.csv(paste0(mydirdi,'SPEET_PREV_STUDIES.csv'))
 #residenze e domicilio per capire se essere pendolari o fuori casa influisce
 home <- read.csv(paste0(mydirdi,'SPEET_RESIDENZE.csv'))
-#esami passati
-exams_passed_aggr <- read.csv(paste0(mydirdi,'speet_exams_passed_aggr.csv'))
-#esami non passati ma con numeri di tentativi
-exams_not_passed_aggr <- read.csv(paste0(mydirdi,'speet_exams_not_passed_aggr.csv'))
-#esami validati
-speet_exams_validated_aggr <- read.csv(paste0(mydirdi,'speet_exams_validated_aggr.csv'))
+
 
 table(degree_desc)
+summary(degree_desc)
+factor (degree_desc$SCOREIMPROVEMENT)
+
+levels(degree_desc$NUMBERATTEMPTSTOBEEVALUATEDONEYEAR)
+
+
+#PA filtra i 3 campi
+library(dplyr)
+degree_desc_ours<-select(degree_desc, starts_with('DEGREEID', 'CDS_POLI_EDU_FLD','DEGREENATURE'))
+c(degree_desc$CDS_ID, degree_desc$DEGREENATURE)
+
+factor(degree_desc$NUMBERATTEMPTSTOENROLESUBJECT)
+#NUMBERATTEMPTSTOENROLESUBJECT
+#NUMBERATTEMPTSTOBEEVALUATEDONEYEAR
+#SCOREIMPROVEMENT
+#degree_desc = degree_desc[] togliere colonne inutili
+degree_desc_ours<-degree_desc (DEGREEID, CDS_POLI_EDU_FLD, DEGREENATURE)
+head(degree_desc_ours)
