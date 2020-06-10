577f55b52f0c96626946a1284236ee6f79cd445e
Pa_O_La <paola.mussida@gmail.com>
2020-06-10 12:38:51 +0200

    data preparation

--- 2_model.R
+++ 2_model.R
@@ -5,10 +5,20 @@ dataset <- read.csv(paste0(mydirdo,'dataset.csv'))
 library(tidyverse)
 library(caret)
 
+# Dataset description to set correct type
+str(dataset)  
+dataset$CARR_AN_ID <- as.character(dataset$CARR_AN_ID)
+dataset$CARR_INGR_AA <- as.factor(dataset$CARR_INGR_AA)
+dataset$PERS_NAS_YYYY <- as.factor(dataset$PERS_NAS_YYYY)
+dataset$PERS_CITT_STT_ID <- as.factor(dataset$PERS_CITT_STT_ID)
+
+
 # We remove the careers with CARR_INGR_AA==2019 because we do not have data to
 # train the model
 dataset_2019 <- dataset[dataset$CARR_INGR_AA==2019, ]
-dataset_2019_excluded <- dataset[dataset$CARR_INGR_AA<2019, ]
+dataset_2019_excluded <- dataset[dataset$CARR_INGR_AA!=2019, ]
+
+dataset_until_2016 <- dataset[dataset$CARR_INGR_AA!=2019 & dataset$CARR_INGR_AA!=2018 & dataset$CARR_INGR_AA!=2017, ]
 
 # Split the data into training and test set
 set.seed(123)
