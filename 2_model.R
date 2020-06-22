b6ce295c1183d66d19d7f6a06409d56e93f701e6
Riccardo Bertoglio <ticio94@hotmail.it>
2020-06-22 19:03:09 +0200

    pr roc curves and model evaluation

--- 2_model.R
+++ 2_model.R
@@ -5,6 +5,8 @@ dataset <- read.csv(paste0(mydirdo,'dataset.csv'))
 library(tidyverse)
 library(caret)
 library(lme4)
+library(mlbench)
+library(party)
 
 # Dataset description to set correct type
 str(dataset)  
@@ -26,19 +28,67 @@ dataset_until_2016 <- droplevels(dataset_until_2016)
 dataset_until_2016$STATUS <- ifelse(dataset_until_2016$STATUS=='DE',1,0)
 dataset_until_2016$STATUS <- as.logical(dataset_until_2016$STATUS)
 
+# Features Selection
+# ensure the results are repeatable
+set.seed(7)
+# calculate correlation matrix
+correlationMatrix <- cor(dataset_until_2016[,c("CARR_ING_ETA","TIT_CONS_VOTO","CFU_PASSATI",
+                                               "MEDIA_PESATA","FAILED_CFU",
+                                               "STUD_AMM_VOTO_REPLACED_MEDIAN")])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+# Rank the features
+# prepare training scheme
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(STATUS~., data=dataset_until_2016, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+
+dataset_until_2016 <- dataset_until_2016[,c(1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,8)]
+# Features selection by RFE
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(dataset_until_2016[,1:18], dataset_until_2016[,19], sizes=c(1:18), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
+# Features random forest
+cf1 <- cforest(STATUS~. , data= dataset_until_2016[,c(2:19)], control=cforest_unbiased(mtry=2,ntree=50))
+
+
 # Split the data into training and test set
 set.seed(123)
 training.samples <- createDataPartition(dataset_until_2016$STATUS, p = 0.8, list = FALSE)
 train.data  <- dataset_until_2016[training.samples, ]
 test.data <- dataset_until_2016[-training.samples, ]
 
-
+# we leave CFU_PASSATI out because it's correlated to MEDIA_PESATA (0.79)
 # Build the model - Basic Linear Regression
-basic_linear_regression.lm <- glmer(STATUS~(1|COURSE)+CARR_INGR_AA+PERS_GENERE+
-                                      PERS_CITT_STT_ID+CARR_INGR_FLTP+CARR_ING_ETA+TIT_MED_TP_CD_ELAB+
-                                      HOM_GEO_PRV_DN+CHANGEDEGREE+TIT_MED_GEO_PRV_CD+
-                                      TIT_CONS_VOTO+TAX+CFU_PASSATI+MEDIA_PESATA+
-                                      FAILED_CFU+STUD_AMM_VOTO_REPLACED_MEDIAN, family=binomial, data=train.data)
+basic_linear_regression.lm <- glmer(STATUS~(1|COURSE)+CARR_ING_ETA+
+                                      CHANGEDEGREE+CFU_PASSATI+
+                                      TIT_CONS_VOTO+TAX+
+                                      FAILED_CFU, family=binomial, control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)),data=train.data)
+
+basic_linear_regression.lm <- lm(STATUS~CARR_ING_ETA+
+                                      CHANGEDEGREE+CFU_PASSATI+
+                                      TIT_CONS_VOTO+TAX+
+                                      FAILED_CFU, data=train.data)
+
 
 # Make predictions and compute the R2, RMSE and MAE
 predictions <- model %>% predict(test.data)
